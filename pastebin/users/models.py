4ec14c3930fc871a144e51a0529cc543dec6f143
lewoudar <lewoudar@gmail.com>
2020-06-08 22:03:29 +0200

    fixed an issue with meta naming convention (sqlaclhemy) added group, permission models

--- pastebin/users/models.py
+++ pastebin/users/models.py
@@ -1,8 +1,21 @@
 import bcrypt
-from sqlalchemy import Column, String, Text
+from sqlalchemy import Column, String, Boolean, Table, ForeignKey
+from sqlalchemy.orm import relationship
 from sqlalchemy_utils import EmailType
 
 from pastebin.meta import Base
+
+user_groups = Table(
+    'user_groups', Base.metadata,
+    Column('user_id', ForeignKey('users.id'), primary_key=True),
+    Column('group_id', ForeignKey('groups.id'), primary_key=True)
+)
+
+group_permissions = Table(
+    'group_permissions', Base.metadata,
+    Column('group_id', ForeignKey('groups.id'), primary_key=True),
+    Column('permission_id', ForeignKey('permissions.id'), primary_key=True)
+)
 
 
 class User(Base):
@@ -10,7 +23,12 @@ class User(Base):
     last_name = Column(String(100), nullable=False)
     pseudo = Column(String(100), unique=True, index=True, nullable=False)
     email = Column(EmailType, unique=True, index=True, nullable=False)
-    password_hash = Column(Text, name='password', nullable=False)
+    password_hash = Column(String(200), name='password', nullable=False)
+    admin = Column(Boolean, nullable=False, default=False)
+    # many to many User<->Group
+    groups = relationship('Group', secondary=user_groups, back_populates='users')
+    # non orm property useful for authentication
+    is_authenticated = False
 
     def set_password(self, pw):
         _hash = bcrypt.hashpw(pw.encode('utf8'), bcrypt.gensalt())
@@ -21,3 +39,17 @@ class User(Base):
             expected_hash = self.password_hash.encode('utf8')
             return bcrypt.checkpw(pw.encode('utf8'), expected_hash)
         return False
+
+
+class Group(Base):
+    name = Column(String(100), nullable=False)
+    # many to many Group<->User
+    users = relationship('User', secondary=user_groups, back_populates='groups')
+    # many to many Group<->Permission
+    permissions = relationship('Permission', secondary=group_permissions, back_populates='groups')
+
+
+class Permission(Base):
+    name = Column(String(100), nullable=False)
+    # many to many Permission<->Group
+    groups = relationship('Group', secondary=group_permissions, back_populates='permissions')
