798dae8bff4782e9b9a6aa0c1e25d7409b774837
lewoudar <lewoudar@gmail.com>
2020-06-09 07:19:32 +0200

    added a script shell that embed sqlalchemy models and a database session

--- pastebin/scripts/__init__.py
+++ pastebin/scripts/__init__.py
@@ -1,9 +1,13 @@
+import code
+
 import click
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy_utils import create_database, database_exists
 
 from pastebin.meta import Base
 from pastebin.settings import DATABASE_URL
+from pastebin.users.models import User, Group, Permission
 
 
 def initialize_database():
@@ -18,3 +22,17 @@ def initialize_database():
     engine = create_engine(database_url)
     Base.metadata.create_all(engine)
     click.echo('finished initialization')
+
+
+def shell():
+    """Creates a python interpreter with sqlalchemy models"""
+    engine = create_engine(DATABASE_URL, connect_args={'check_same_thread': False})
+    session_class = sessionmaker(bind=engine)
+    db = session_class()
+    code.interact(banner='Interactive database console', exitmsg='Good bye!', local={
+        'User': User,
+        'Group': Group,
+        'Permission': Permission,
+        'db': db
+    })
+    db.close()
